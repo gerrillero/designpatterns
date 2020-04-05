a22356415c3a920f28a009c1b5212588447f410f
Antonio <antonio75@gmail.com>
2020-04-05 11:37:58 +0200

    Iterator patttern

---
+++ Iterator/BrowserHistory.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Iterator
+{
+    public class BrowserHistory
+    {
+        private List<string> Urls { get; set; }
+
+        public BrowserHistory()
+        {
+            Urls = new List<string>();
+        }
+
+        public void Push(string url)
+        {
+            Urls.Add(url);
+        }
+
+        public string Pop()
+        {
+            var lastUrl = Urls.LastOrDefault();
+            Urls.Remove(lastUrl);
+
+            return lastUrl;
+        }
+
+        public IIterator<string> CreateIterator()
+        {
+            return new ListIterator(this);
+        }
+
+
+        public class ListIterator : IIterator<string>
+        {
+            private readonly BrowserHistory browserHistory;
+            private int index;
+
+            public ListIterator(BrowserHistory browserHistory)
+            {
+                this.browserHistory = browserHistory;
+            }
+
+            public string Current()
+            {
+                return browserHistory.Urls.ElementAt(index);
+            }
+
+            public bool HasNext()
+            {
+                return (index < browserHistory.Urls.Count);
+            }
+
+            public void Next()
+            {
+                index++;
+            }
+        }
+
+
+    }
+}
---
+++ Iterator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Iterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var browserHistory = new BrowserHistory();
+            browserHistory.Push("a");
+            browserHistory.Push("b");
+            browserHistory.Push("c");
+            browserHistory.Push("d");
+            browserHistory.Pop();
+
+            var iterator = browserHistory.CreateIterator();
+
+            while (iterator.HasNext())
+            {
+                var url = iterator.Current();
+                Console.WriteLine(url);
+                iterator.Next();
+            }
+
+
+        }
+    }
+}
