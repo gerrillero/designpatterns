cd6f04e71bcb1637595d35c8779fb3459545fbf4
Antonio <antonio75@gmail.com>
2020-09-19 22:35:45 +0200

    Strategy exercise problem solved

---
+++ StrategyMosh/Exercise/AesEncryptStrategy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StrategyMosh.Exercise
+{
+    public class AesEncryptStrategy : IEncryptStrategy
+    {
+        public void Encrypt(string message)
+        {
+            Console.WriteLine("Encrypting message using AES");
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ StrategyMosh/Exercise/ChatClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StrategyMosh.Exercise
+{
+    public class ChatClient
+    {
+
+        public void Send(String message, IEncryptStrategy encryptStrategy)
+        {
+            encryptStrategy.Encrypt(message);
+            Console.WriteLine("Sending the encrypted message...");
+        }
+    }
+
+}
