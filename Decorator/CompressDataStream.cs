81ff8a329ce4bc373657e2e88ed3f681d527cdbf
Antonio <antonio75@gmail.com>
2021-03-20 22:16:32 +0100

    Decorator pattern

---
+++ Decorator/CompressDataStream.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    public class CompressDataStream : IStream
+    {
+        private IStream _stream;
+
+        public CompressDataStream(IStream stream)
+        {
+            _stream = stream;
+        }
+
+        public void Write(string data)
+        {
+            var compressedData = compressData(data);
+            _stream.Write(compressedData);
+        }
+
+        private string compressData(string data)
+        {
+            return data.Substring(0, 5);
+        }
+    }
+}
---
+++ Decorator/EncriptCloudStream.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    public class EncriptCloudStream : IStream
+    {
+        private IStream _stream;
+
+        public EncriptCloudStream(IStream stream)
+        {
+            _stream = stream;
+        }
+
+        public void Write(string data)
+        {
+            var encryptedtData = encriptData(data);
+            _stream.Write(encryptedtData);
+        }
+
+        private string encriptData(string data)
+        {
+            return "!@#@$#%$^$%$%&2342sgsgs";
+        }
+    }
+}
