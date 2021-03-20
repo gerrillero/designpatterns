1eff95a1d178e53b77607f2c692771852364dd2e
Antonio <antonio75@gmail.com>
2021-03-20 21:16:08 +0100

    Adapter pattern

---
+++ Adapter/ImageView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    public class ImageView
+    {
+        private IFilter _filter;
+
+        public ImageView(IFilter filter)
+        {
+            _filter = filter;
+        }
+
+        public void Apply(Image image)
+        {
+            _filter.Apply(image);
+        }
+    }
+}
