eccce13a9cb8ccc25a5d25ea6c043e64aebdcb88
Antonio <antonio75@gmail.com>
2021-03-21 11:14:55 +0100

    Facade the problem

---
+++ Facade/NotificationServer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade
+{
+    public class NotificationServer
+    {
+        //connect() -> Connection
+        // authenticate(appID, key) -> AuthToken
+        // send(authToken, message, target)
+        // conn.Disconect()
+
+        public Connection Connect(string ipAddress)
+        {
+            return new Connection();
+        }
+
+        public AuthToken Authecticate(string appID, string key)
+        {
+            return new AuthToken();
+        }
+
+        public void Send(AuthToken authToken, Message message, string target)
+        {
+            Console.WriteLine("Sended!");
+
+        }
+         
+    }
+}
---
+++ Facade/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new NotificationServer();
+            var connection = server.Connect("0.0.0.0");
+            var authentification = server.Authecticate("appID", "key");
+            var message = new Message("message content");
+            server.Send(authentification, message, "target1");
+            connection.Disconnect();
+
+        }
+    }
+}
