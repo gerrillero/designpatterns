e328c49d82a9c7a01ee796a856458a34e84a4ba0
Antonio <antonio75@gmail.com>
2021-05-10 19:56:08 +0200

    Composite pattern

---
+++ Composite/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var group1 = new Group();
+            group1.Shapes.Add(new Shape());
+            group1.Shapes.Add(new Shape());
+
+            var group2 = new Group();
+            group2.Shapes.Add(new Shape());
+            group2.Shapes.Add(new Shape());
+
+
+        }
+    }
+}
---
+++ Composite/Shape.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    public class Shape
+    {
+        public void Render()
+        {
+            Console.WriteLine("Render shape");
+        }
+    }
+}
